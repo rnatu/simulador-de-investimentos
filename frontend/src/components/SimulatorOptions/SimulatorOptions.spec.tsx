9842d214597623dae710258bc5da9f09d58a3702
Renato Xavier <rnatu91@gmail.com>
2022-04-19 15:33:41 -0300

    test: add SimulatorOptions test

--- frontend/src/components/SimulatorOptions/SimulatorOptions.spec.tsx
+++ frontend/src/components/SimulatorOptions/SimulatorOptions.spec.tsx
@@ -4,17 +4,18 @@ import { SimulatorOptions } from '.';
 describe('SimulatorOptions Component', () => {
   const handleOptionMock = jest.fn();
 
-  const { getByRole } = render(
-    <SimulatorOptions
-      title="Rendimento"
-      buttonsTitle={['Button test 1', 'Button test 2']}
-      defaultButton="Button test 2"
-      handleOption={handleOptionMock}
-    />,
-  );
-
   it('renders correctly', () => {
+    const { getByRole } = render(
+      <SimulatorOptions
+        title="Rendimento"
+        buttonsTitle={['Button test 1', 'Button test 2']}
+        defaultButton="Button test 2"
+        handleOption={handleOptionMock}
+      />,
+    );
+
     expect(getByRole('heading', { name: 'Rendimento' }));
+    expect(getByRole('img', { name: /informação sobre rendimento/i }));
     expect(getByRole('button', { name: 'Button test 1' }));
     expect(getByRole('button', { name: 'Button test 2' }));
   });
